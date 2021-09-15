1c0c6cce3658caefc6f335db428e37f1b0e8ee86
Mauro <mauroyamasaki2012@gmail.com>
2021-09-15 08:42:44 -0300

    Definir os Models de polls

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Question(models.Model):
+    question_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('data publicação')
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    choice_id=models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+        
